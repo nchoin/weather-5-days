615a4856457ba98c5ae0d35319b0cb20de33ca8d
nchoin <139597297+nchoin@users.noreply.github.com>
2023-10-21 23:41:44 -0400

    added beginning coding using the API. the APIURL is the sample with London as the city. Used this to set up the text.Content for each weather condition. Still need to figure out how the search for the name of the city. How do I Target that in the API since 5day has a lon/lat query?

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,31 @@
-// const weatherApiUrl = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={fc0cfae3b133613dbc3178be6b4c1a4d}"
-const weatherApiUrl = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={https://api.weather.gov/points/{latitude},{longitude}}"
+// const weatherApiUrl = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=fc0cfae3b133613dbc3178be6b4c1a4d"
+// const weatherApiUrl = "https://goweather.herokuapp.com/weather/{city}"
+const todayIs = Date
 
-async function getWeatherForecast() {
+// Change the url to the 5day forcast option - delete my API key and make the a separate variable
+// const weatherURL = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}"
+// const apiKey = &appid=fc0cfae3b133613dbc3178be6b4c1a4d
+const weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=fc0cfae3b133613dbc3178be6b4c1a4d"
+async function getWeatherForecast(city) {
+    // change the const data = await response.fetch(weatherURL + city + apiKey);
     const response = await fetch(weatherApiUrl);
     const data = await response.json();
     console.log(data);
+    // even though I set the API to imperial measurement, the temp is coming in the document as Kelvin. I added the conversions for F and C
+    const tempFC = Math.round(data.main.temp *((9/5)) - 459.67)+"°F/" +Math.round(data.main.temp -273.15).toFixed(0)+"°C"
     
+    document.querySelector("#city").textContent = data.name
+    document.querySelector("#temp").textContent = tempFC
+    document.querySelector("#humidity").textContent = "Humidity is " + data.main.humidity 
+    document.querySelector("#windSpeed").textContent = "Wind Speed  is "+ data.wind.speed + " mph"
+    document.querySelector("#weatherConditionIcon").textContent = data.weather[0].main
+
 }
 getWeatherForecast();
-
+// refresh every 10 minutes based on weather documentation saying it refreshes weather information every 10 minutes
+// setInterval(getWeatherForecast,600000)
+{/* <div id="city"></div>
+        <div id="temp">°</div>
+        <div id="humidity"></div>
+        <div id="windSpeed"></div>
+        <div id="weatherConditionIcon"></div> */}
